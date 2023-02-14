e3611c446beb7bd9d5d01ae79b156a23c1b2376a
Chiayo Lin <chiayo.lin@gmail.com>
2023-02-14 09:22:01 -0500

    fix next hydration mismatch issue

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -2,12 +2,14 @@ export function formatDate(date: string | undefined) {
   if (!date)
     return undefined;
 
-  return (new Date(date)).toLocaleDateString(
-    'en-US',
-    {
-      year: 'numeric',
-      month: 'short',
-      day: 'numeric',
-    },
-  );
+  const MONTH = [
+    'Jan', 'Feb', 'Mar', 'Apr',
+    'May', 'Jun', 'Jul', 'Aug',
+    'Sep', 'Oct', 'Nov', 'Dec'
+  ];
+
+  const [y, m, d] = date.slice(0, 10).split('-');
+
+  return `${MONTH[Number(m) - 1]} ${d}, ${y}`;
+
 }
