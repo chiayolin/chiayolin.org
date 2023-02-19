e07f49861d97ce680a177aa20f1a34d694f60322
Chiayo Lin <chiayo.lin@gmail.com>
2023-02-19 18:32:06 -0500

    add profile to readme section

--- src/pages/readme/index.tsx
+++ src/pages/readme/index.tsx
@@ -1,7 +1,8 @@
 import Layout from "@/components/layout";
-import { Heading } from "@chakra-ui/react";
+import { Box, Heading } from "@chakra-ui/react";
 import { getPostData } from "@/lib/weblog";
 import MDXComponents from '@/components/mdx-components';
+import { Image } from '@/components/prose';
 import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
 
 export async function getStaticProps() {
@@ -22,6 +23,21 @@ export default function Readme({ mdxSource }: {
   return (
     <Layout>
       <Heading mb='5' size='md' fontFamily='mono'>{frontmatter?.title}</Heading>
+      <Box
+        mb='5'
+        mx='auto'
+        boxSize='15rem'
+        overflow='hidden'
+        userSelect='none'
+        borderRadius='full'
+      >
+        <Image
+          transform='scale(1.09)'
+          objectPosition='20% 0'
+          src={frontmatter?.cover}
+          alt={frontmatter?.caption}
+        />
+      </Box>
       <MDXRemote
         {... {compiledSource}}
         components={MDXComponents}
